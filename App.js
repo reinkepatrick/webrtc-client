a25a1dacfef61ea77fef76d121727b4eb786bbec
Patrick Reinke <patrick.reinke@fh-bielefeld.de>
2019-11-12 15:17:40 +0100

    finish socket.io

--- App.js
+++ App.js
@@ -7,15 +7,49 @@
  */
 
 import React from 'react';
-import {StyleSheet, View, Text, StatusBar} from 'react-native';
+import {StyleSheet, View, Text, Button, StatusBar} from 'react-native';
+import io from 'socket.io-client';
+import {SERVER_URL} from './config';
 
 class App extends React.Component {
+  socket = null;
+  state = {
+    buttonPressed: 0,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.socket = io(SERVER_URL, {
+      reconnection: true,
+      reconnectionDelay: 500,
+      jsonp: false,
+      reconnectionAttempts: Infinity,
+      transports: ['websocket'],
+    });
+
+    this.socket.on('button_pressed', data => {
+      this.setState({
+        buttonPressed: data,
+      });
+    });
+
+    this.socket.on('connect_error', err => {
+      console.log(err);
+    });
+  }
+
+  onClick = () => {
+    this.socket.emit('button_pressed', this.state.buttonPressed);
+  };
+
   render() {
     return (
       <>
         <StatusBar barStyle="dark-content" />
         <View style={styles.container}>
-          <Text style={styles.text}>Test</Text>
+          <Text style={styles.text}>{this.state.buttonPressed}</Text>
+          <Button title="Gib mir Pi" onPress={this.onClick} />
         </View>
       </>
     );
--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-export const SERVER_URL = '192.168.1.101:3000';
+export const SERVER_URL = 'http://192.168.1.103:3000';
